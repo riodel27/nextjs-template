541f9090551573a7db48518deabca64842c756df
riodel27 <riopamaong@gmail.com>
2023-07-06 23:37:02 +0800

    feat: Set up Cypress for end-to-end testing

---
+++ cypress/global.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface CustomWindow extends Window {}
+
+  interface Chainable {
+    login(email: string, password: string): void;
+    register(user: { name: string; email: string; password: string }): void;
+  }
+}
